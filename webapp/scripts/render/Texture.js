829570f41df0f0c1bdd6761a1fb0a33bb4684cbd
kbirk <birk.kevin@gmail.com>
2016-04-24 14:01:59 -0400

    Add support for non POT textures.

--- webapp/scripts/render/Texture.js
+++ webapp/scripts/render/Texture.js
@@ -11,6 +11,35 @@
         9987: true
     };
 
+    function nextHighestPowerOfTwo( num ) {
+        var i;
+        if ( num !== 0 ) {
+            num = num-1;
+        }
+        for ( i=1; i<32; i<<=1 ) {
+            num = num | num >> i;
+        }
+        return num + 1;
+    }
+
+    function isPowerOfTwo( num ) {
+        return ( num !== 0 ) ? ( num & ( num - 1 ) ) === 0 : false;
+    }
+
+    function resizeToPowerOfTwo( img ) {
+        if ( isPowerOfTwo( img.width ) && isPowerOfTwo( img.height ) ) {
+            return img;
+        }
+        // create an empty canvas element
+        var canvas = document.createElement( 'canvas' );
+        canvas.width = nextHighestPowerOfTwo( img.width );
+        canvas.height = nextHighestPowerOfTwo( img.height );
+        // copy the image contents to the canvas
+        var ctx = canvas.getContext( '2d' );
+        ctx.drawImage( img, 0, 0, img.width, img.height, 0, 0, canvas.width, canvas.height );
+        return canvas;
+    }
+
     function Texture( args ) {
         var gl = this.gl = context();
         this.target = args.target;
@@ -19,6 +48,8 @@
         this.type = args.type;
         this.texture = gl.createTexture();
         gl.bindTexture( this.target, this.texture );
+        // resize to POT
+        args.image = resizeToPowerOfTwo( args.image );
         // buffer the texture
         gl.texImage2D(
             this.target,
